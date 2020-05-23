7db0dff2075c7d9db20523b46a9ed60c80d6c17c
IsmaelSMR <65824169+IsmaelSMR@users.noreply.github.com>
2020-05-23 17:18:43 -0600

    Add files via upload
    Cambios en la interfaz del menú y añadido en clientes.

---
+++ PROYECTOFINAL-master/AgregarClientes.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fstream>
+#include "Clientes.h"
+
+using namespace std;
+
+void agregarCliente()
+{
+    Cliente cliente1;
+    ofstream datos;
+    datos.open("clientes.txt",ios::app); //Abre el archivo en modo de edición
+
+    /*
+  (Nombre, # de cedula, direccion, telefono celular, género)
+Habitaciones que posee el hotel: Existen hoteles con cuartos costosos (ejecutivos), económicos, etc.
+Agregar la habitación que adquirió el cliente, cuántos días estará y cuánto le costará.
+
+    */
+
+
+
+
+    cout<<"Nuevo Registro de cliente"<<endl;
+    cout<<"Cedula: ";
+    gets(cliente1.Cedula);
+    fflush(stdin);
+
+    cout<<"Nombre: ";
+    gets(cliente1.Nombre);
+    fflush(stdin);
+
+    cout<<"Telefono: ";
+    gets(cliente1.Telefono);
+    fflush(stdin);
+
+    cout<<"Direccion: ";
+    gets(cliente1.Direccion);
+    fflush(stdin);
+
+    cout<<"Correo: ";
+    gets(cliente1.Correo);
+    fflush(stdin);
+
+    cout<<"Genero" ;
+    gets(cliente1.Genero);
+    fflush(stdin);
+
+    cout<<"Habitacion" ;
+    gets(cliente1.Habitacion);
+    fflush(stdin);
+
+    cout<<"Estadia" ;
+    gets(cliente1.Estadia);
+    fflush(stdin);
+
+    fflush(stdout);
+
+    datos<<cliente1.Cedula<<endl;
+    datos<<cliente1.Nombre<<endl;
+    datos<<cliente1.Telefono<<endl;
+    datos<<cliente1.Direccion<<endl;
+    datos<<cliente1.Correo<<endl;
+    datos<<cliente1.Genero<<endl;
+    datos<<cliente1.Habitacion<<endl;
+    datos<<cliente1.Estadia<<endl;
+
+
+    datos.close();
+}
+
+void listarCliente()
+{
+    // 1 - Abrir el archivo
+    // 2 - Leer el archivo
+    // 3 - Mostrar el contenido del archivo
+
+    ifstream lectura;
+    string texto; //aqui es donde guardaré todo el contenido del archivo
+
+    lectura.open("clientes.txt", ios::in); //Abrimos el archivo en modo Lectura
+
+    if(lectura.fail()) // Si falla la apertura del archivo
+    {
+        cout<<"No se pudo abrir el archivo"<<endl;
+        exit(1);
+    }
+
+    while(!lectura.eof()) //eof -> End of File ... Mientras no sea el final del archivo
+    {
+        getline(lectura, texto);
+        cout<<texto<<endl;
+    }
+
+    lectura.close();
+
+}
---
+++ PROYECTOFINAL-master/Clientes.h
@@ -0,0 +1,17 @@
+#ifndef CLIENTE_H_INCLUDED
+#define CLIENTE_H_INCLUDED
+
+typedef struct
+{
+    char Cedula[20];
+    char Nombre[50];
+    char Telefono[20];
+    char Correo[20];
+    char Direccion[100];
+    char Genero[100];
+    char Habitacion[100];
+    char Estadia[100];
+
+}Cliente;
+
+#endif // CLIENTE_H_INCLUDED
